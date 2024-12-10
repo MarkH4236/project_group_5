a5e506cdbe4132cb6a8bce7c036e1b0f8b92f37f
MarkH4236 <mark.horkriengkrai@gmail.com>
2024-12-10 10:21:08 -0700

    first commit

---
+++ app/page.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import signup from "./signup/page";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold mb-4">
+        Welcome to the BMI Calculator!
+      </h1>
+      <p className="text-lg mb-6">Track how fat you are!!!!</p>
+      <Link href="/signup">
+        <button className="p-2 bg-blue-500 text-white rounded">
+          Go to signup
+        </button>
+      </Link>
+    </div>
+  );
+}
